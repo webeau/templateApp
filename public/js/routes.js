d6d17504708a28d4666c7b338d92b6cf3d7ddb84
Carl Ripa <cj.ripa@gmail.com>
2013-07-13 21:29:18 +0200

    Fixed a bug which broke AngularJS when optimizing the javascript through RequireJS. Specific Angular-providers must ALWAYS be injected (i.e )

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,34 +1,41 @@
 define(['globals'], function (globals) {
     'use strict';
 
+    var routeConfig = ['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        
+        $routeProvider.when('/', //here we can add params like, :oneParam
+            {
+                templateUrl: 'templates/mainView.html',
+                //controller: 'MainViewCtrl' //use when only one controller exists in the template
+                resolve: {
+                    testData: ['$q', '$route', 'exampleService', function ($q, $route, exampleService) {
+                        var deferred = $q.defer();
+                        exampleService.getWithPromise() //here we can send in data from the routeparams or querystring, example: $route.current.pathParams.oneParam
+                            .then(function(data){ 
+                                deferred.resolve(data); 
+                            });
+                        return deferred.promise;
+                    }]
+                }
+            });
+
+        $routeProvider.when('/about',
+            {
+                templateUrl: 'templates/about.html'
+            });
+
+        $routeProvider.when('/contact',
+            {
+                templateUrl: 'templates/contact.html'
+            });
+
+        //all other routes redirects to /
+        $routeProvider.otherwise({redirectTo: '/'});
+
+        //in order to get nicer routes without #.. remove if old browsers are supported
+        $locationProvider.html5Mode(true);
+    }];
+
     return angular.module(globals.appName + '.routes', [])
-        .config(function ($routeProvider, $locationProvider) {
-            $routeProvider.when('/', //here we can add params like, :oneParam
-                {
-                    templateUrl: 'templates/mainView.html',
-                    //controller: 'MainViewCtrl' //use when only one controller exists in the template
-                    resolve: {
-                        testData: function ($q, $route, exampleService) {
-                            var deferred = $q.defer();
-                            exampleService.getWithPromise() //here we can send in data from the routeparams or querystring, example: $route.current.pathParams.oneParam
-                                .then(function(data){ deferred.resolve(data); });
-                            return deferred.promise;
-                        }
-                    }
-                });
-            $routeProvider.when('/about',
-                {
-                    templateUrl: 'templates/about.html'
-                });
-            $routeProvider.when('/contact',
-                {
-                    templateUrl: 'templates/contact.html'
-                });
-
-            //all other routes redirects to /
-            $routeProvider.otherwise({redirectTo: '/'});
-
-            //in order to get nicer routes without #.. remove if old browsers are supported
-            $locationProvider.html5Mode(true);
-        });
+        .config(routeConfig);
 });
--- public/js/services.js
+++ public/js/services.js
@@ -1,34 +1,37 @@
 define(['globals'], function (globals) {
     'use strict';
 
+    var exampleService = ['$rootScope', '$resource', '$q', function ($rootScope, $resource, $q) {
+        var service = {};
+        service.text = '';
+
+        service.broadcastText = function (text) {
+            this.text = text;
+            $rootScope.$broadcast('broadcastText');
+        };
+
+        service.getWithPromise = function () {
+            var deferred = $q.defer();
+
+            $resource('/api/test', {})
+                .get({},
+                function (data) {
+                    //success
+                    deferred.resolve(data);
+                },
+                function (response) {
+                    //error
+                    deferred.reject(response);
+                });
+
+            return deferred.promise;
+        };
+
+        return service;
+    }];
+
+    //returns the services
     return angular.module(globals.appName + '.services', ['ngResource'])
-        .factory('exampleService', function ($rootScope, $resource, $q) {
-            var service = {};
-            service.text = '';
-
-            service.broadcastText = function (text) {
-                this.text = text;
-                $rootScope.$broadcast('broadcastText');
-            };
-
-            service.getWithPromise = function () {
-                var deferred = $q.defer();
-
-                $resource('/api/test', {})
-                    .get({},
-                    function (data) {
-                        //success
-                        deferred.resolve(data);
-                    },
-                    function (response) {
-                        //error
-                        deferred.reject(response);
-                    });
-
-                return deferred.promise;
-            };
-
-            return service;
-        })
+        .factory('exampleService', exampleService)
         .value('version', '0.1');
 });
